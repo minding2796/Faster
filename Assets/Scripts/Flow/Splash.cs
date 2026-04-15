887f24be280647d7934dfe7a32ed81ba02dcb950
minding2796 <minding2796@gmail.com>
2026-04-15 11:33:04 +0900

    feat: add Splash Scenes

---
+++ Assets/Scripts/Flow/Splash.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace Flow
+{
+    public class Splash : MonoBehaviour
+    {
+        public VideoPlayer videoPlayer;
+        private bool started;
+        
+        public void OnAnyKey(InputValue value)
+        {
+            if ((int)value.Get<float>() != 1) return;
+            SceneManager.LoadScene("Title");
+        }
+
+        private void Update()
+        {
+            switch (videoPlayer.isPlaying)
+            {
+                case true:
+                    started = true;
+                    break;
+                case false when started:
+                    SceneManager.LoadScene("Title");
+                    break;
+            }
+        }
+    }
+}
